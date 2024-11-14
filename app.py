6e74bbd340771da3e75b52362152787299653e56
Lucas Henrique de Barros <128721960+lucashebarros@users.noreply.github.com>
2024-11-14 09:51:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -60,11 +60,11 @@ st.table(df)
 st.write("""
 **Economias Potenciais:**
 
-- **Economia Mensal Atual**: "R$ 297,50 - R$ 130,00" = **R$ 167,50**
-- **Economia Anual Atual**: "R$ 3.570,00 - R$ 1.560,00" = **R$ 2.010,00**
+- **Economia Mensal Atual**: 297,50 - 130,00" = **R$ 167,50**
+- **Economia Anual Atual**: 3.570,00 - 1.560,00" = **R$ 2.010,00**
 
-- **Economia Mensal Futura (a partir de Abril)**: "R$ 406,50 - R$ 130,00" = **R$ 276,50**
-- **Economia Anual Futura**: "R$ 4.878,00 - R$ 1.560,00" = **R$ 3.318,00**
+- **Economia Mensal Futura (a partir de Abril)**: 406,50 - 130,00" = **R$ 276,50**
+- **Economia Anual Futura**: 4.878,00 - 1.560,00" = **R$ 3.318,00**
 """)
 
 # Recursos adicionais
